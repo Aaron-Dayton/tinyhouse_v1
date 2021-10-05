db8da27ca094f52d0936f276100947c0c865a2dc
Aaron Dayton <aaron@ruboss.com>
2021-10-05 12:11:07 -0700

    added mock database population script and modified resolvers to query and mutate the actual database

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,19 +1,38 @@
 import { IResolvers } from "@graphql-tools/utils";
-import { listings } from "../listings";
+import { Database, Listing } from "../lib/types";
+import { ObjectId } from "mongodb";
 
 export const resolvers: IResolvers = {
   Query: {
-    listings: () => { return listings}
+    listings: async (
+      _root: undefined,
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<Listing[]> => {
+      return await db.listings.find({}).toArray();
+    }
   },
   Mutation: {
-    deleteListing: (_root: undefined, { id }: { id: string }) => {
-      for (let ctr = 0; ctr < listings.length; ctr++) {
-        if (listings[ctr].id === id) {
-          return listings.splice(ctr, 1)[0]
-        }
-      }
+    deleteListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ): Promise<Listing> => {
+      const res = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id)
+      });
 
-      throw new Error("Failed to delete listing")
+      if (!res.value) {
+        throw new Error("Failed to delete listing");
+      };
+
+      return res.value;
     }
+  },
+  // All resolvers needed for Listing, besides id which has a different name and
+  // type in types.ts and typeDefs.ts, are trivial (same name and type), so we
+  // do not need to define them.
+  Listing: {
+    id: (listing: Listing): string => listing._id.toString()
   }
 };
